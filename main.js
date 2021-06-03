dae21240e8b77d6d0c003173f928f70a2e1b2b21
Piotr Polanski <polanski.pio@gmail.com>
2021-06-03 19:34:53 +0900

    my profile page

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // typing animation 
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "LeWagon's Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    })
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "LeWagon's Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    })
+});
